55aef9cf5af6e36a05c54f80dbc82d2a500548ab
bogdanolszewski <74648319+bogdanolszewski@users.noreply.github.com>
2022-05-17 10:57:07 +0200

    Update update_disprod_db_shippings_hist_v2.py
    fixed error where only 4 plants got pushed to SQL.

--- update_disprod_db_shippings_hist_v2.py
+++ update_disprod_db_shippings_hist_v2.py
@@ -29,6 +29,7 @@ df1 = df1.drop(["SHIP FROM", "DELIVERY TO", "ITEM ID", "GIT", "GIT STATUS",
          "BUYER","SELLER","DESP. ADV. N.","ASSET ID", "DOC. ID", "NOTE"], axis=1)
 print(df1)
 
+
 df2 = pd.read_excel(sapfiles)
 df2['Date ADJ'] = np.where(df2['Posting Date'].dt.dayofweek==7, df2['Posting Date']-timedelta(days=1), df2['Posting Date'])
 df2['Qty'] = abs(df2["Qty in Un. of Entry"])
@@ -50,31 +51,41 @@ df3['test2'] = np.where(df3['Date ADJ']==td, "NOK", "OK")
 
 df3 = df3[(df3['test']=="OK") & (df3['test2']=="OK")]
 df3 = pd.DataFrame(df3.groupby(["Plant","Date ADJ"]).agg({"Shipping": "sum"}).reset_index())
+df3 = df3.rename(columns = {"Date ADJ": "Date"})
+print(df3)
+
 #---
 df4 = pd.read_excel(factoryperf)
 df4['Date'] = pd.to_datetime(df4['Date'])
 df4 = df4.drop(["Factory", "GPH Sub Group", "Old Sub Group", "Item GR", "Planned", "KPI03", "Delta", "DSA Code",
           "DSA Extra Reason", "ANC Component Code", "Factory Supplier", "Sales Company", "Equipment Code",
           "Action Description", "Action Responsible", "Action Status"], axis=1)
+
 df4['Plant'] = df4['Node'].map({"PLT":"PL31","PLV":"PL21","PLY":"PL41","PLS":"PL11","ROB":"RO03",
                                 "ZS":"IT24","ZP":"IT04","ZM":"IT34","ZO":"IT44","UKE":"UA01","HUC":"HU01",
                                 "HUY":"HU11","DGT":"DE02","DMU":"DE02","PLB":"PL91","ITB":"IT94"})
 df4 = df4.dropna(axis=0)
 df4 = pd.DataFrame(df4.groupby(["Plant","Date"]).agg({"Achieved": "sum"}).reset_index())
 
-df3 = df3[df3['Date ADJ']==(date.today()-timedelta(days=1)).strftime('%Y-%m-%d')]
+df3 = df3[df3['Date']==(date.today()-timedelta(days=1)).strftime('%Y-%m-%d')]
+df3['Shipping'] = df3['Shipping'].astype(np.int64)
 df4 = df4[df4['Date']==(date.today()-timedelta(days=1)).strftime('%Y-%m-%d')]
+#print(df3.head(15),"\n"); print(df4.head(15),"\n")
+df5 = df4.merge(df3, on="Plant", how = "left")
+df5 = df5.merge(df3, on="Plant", how = "outer")
+df5 = df5.drop(columns=["Date_y", "Shipping_x","Date"])
+df5["Date_x"] = df5["Date_x"].fillna(td-timedelta(days=1))
+df5["Achieved"] = df5["Achieved"].fillna(0)
+df5["Shipping_y"] = df5["Shipping_y"].fillna(0)
+df5 = df5.rename(columns={"Date_x": "Date", "Shipping_y": "Shipping"})
+df5["Date"] = pd.to_datetime(df5["Date"])
 
-df5 = pd.merge(df3, df4, on = "Plant", how = "left")
-df5 = df5.drop(columns=["Date"])
-df5['Achieved'] = df5['Achieved'].fillna(0)
 df5['Shipping'] = df5['Shipping'].astype(np.int64)
 df5['Achieved'] = df5['Achieved'].astype(np.int64)
-df5['weekday'] = (df5['Date ADJ'].dt.dayofweek)+1 #+1 because week starts from 0(monday)
+df5['weekday'] = (df5['Date'].dt.dayofweek)+1 #+1 because week starts from 0(monday)
 df5['weekday'] = df5['weekday'].astype(np.int64)
-df5 = df5.iloc[:, [0,1,2,3,4]]
-print(df5.head(5))
-
+df5 = df5.iloc[:, [0,1,3,2,4]]
+print(df5)
 
 server='DP-SupplyPlanning-EU.biz.electrolux.com'
 db='SupplyPlanning'
@@ -94,7 +105,7 @@ INSERT INTO [dbo].[data_disprod_shippings_history](
 for i in range(0, len(df5)):
     dfvals = "'"+str(df5.iloc[i,0])+"','"+str(df5.iloc[i,1].strftime('%Y-%m-%d'))+"','"+str(df5.iloc[i,2])+"','"+str(df5.iloc[i,3])+"','"+str(df5.iloc[i,4])+"', '"+today+"'"
     sqlstr = sqlstr+dfvals+")"
-    print(sqlstr)
+    #print(sqlstr)
     cursor.execute(sqlstr)
     conn.commit()
     sqlstr = """
